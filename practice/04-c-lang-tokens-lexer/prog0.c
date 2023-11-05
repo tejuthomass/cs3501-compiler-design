4d2ee64671f7ffce9494ac7f37d27475637a14c0
Teju Thomas <thomasteju7@gmail.com>
2023-11-05 09:21:07 -0800

    Added 4 practice exercises

---
+++ practice/04-c-lang-tokens-lexer/prog0.c
@@ -0,0 +1,7 @@
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    printf("The sum is %d", sum);
+    return 0;
+}
---
+++ practice/04-c-lang-tokens-lexer/prog1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int number = 5;
+    printf("Factorial of %d is %d", number, factorial(number));
+    
+    for (int i = 0; i < 10; i++) {
+        if (i % 2 == 0) {
+            printf("%d is even\n", i);
+        } else {
+            printf("%d is odd\n", i);
+        }
+    }
+    
+    return 0;
+}
